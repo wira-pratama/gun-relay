dc848f7d0738cdab196dcfa11579a26a1d787874
wira-pratama <adiwira85pratama@gmail.com>
2024-08-23 01:10:20 +0700

    new try

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const GUN = require('gun');
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-    res.write('This is a Gun.js Relay for gunfp');
-    res.end();
+// const server = http.createServer((req, res) => {
+//     res.write('This is a Gun.js Relay for gunfp');
+//     res.end();
+// }).listen(8080);
+
+http.createServer((request, response) => {
+    request.on('error', (err) => {
+        console.error(err);
+    }).on('end', () => {
+        response.on('error', (err) => {
+            console.error(err);
+        });
+
+        response.writeHead(200, {
+            "Content-Security-Policy": "default-src *"
+        });
+
+        response.end("<html><body><h1>Hello, Security Headers!</h1></body></html>");
+    });
 }).listen(8080);
-const gun = GUN({web: server});
+const gun = GUN({ web: server });
